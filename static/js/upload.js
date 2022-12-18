915dd06f50d415b8745002264f61d3087b7f64fd
Yarne Gooris <yarne.gooris@gmail.com>
2022-12-18 15:11:42 +0100

    Updated

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,7 +1,7 @@
-document.querySelector('body').addEventListener('dragover', function() {
-    document.querySelector('.upload-div').style.border = '2px solid #000';
+document.querySelector('.upload-div').addEventListener('dragover', function() {
+    document.querySelector('.upload-div').classList.add('upload-dragover');
 });
 
-document.querySelector('body').addEventListener('dragleave', function() {
-    document.querySelector('.upload-div').style.border = '2px dashed #2a2d3a';
+document.querySelector('.upload-div').addEventListener('dragleave', function() {
+    document.querySelector('.upload-div').classList.remove('upload-dragover');
 });
