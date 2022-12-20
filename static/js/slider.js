3c2e027f5e4362411707cad67d7597d669507874
Yarne Gooris <yarne.gooris@gmail.com>
2022-12-20 16:46:57 +0100

    Updated

--- static/js/slider.js
+++ static/js/slider.js
@@ -8,9 +8,10 @@ document.querySelectorAll('.ready2go-card').forEach(anchor => {
 
 document.querySelectorAll('.modal-dialog-scrollable .modal-body').forEach(anchor => {
     anchor.addEventListener('scroll', function(e){
-        var modal_scrollTop = $('.modal-dialog-scrollable .modal-body')[9].scrollTop;
-        var modal_scrollHeight = $('.modal-dialog-scrollable .modal-body')[9].scrollHeight;        
-        var modal_innerHeight = $('.modal-dialog-scrollable .modal-body')[9].offsetHeight;        
+        var modal_body = $('.modal-dialog-scrollable .modal-body')[9];
+        var modal_scrollTop = modal_body.scrollTop;
+        var modal_scrollHeight = modal_body.scrollHeight;        
+        var modal_innerHeight = modal_body.offsetHeight;        
         
         if (modal_scrollTop + modal_innerHeight >= modal_scrollHeight) {
             document.querySelector('.scroll-shadow-bottom').style.display = 'none';
